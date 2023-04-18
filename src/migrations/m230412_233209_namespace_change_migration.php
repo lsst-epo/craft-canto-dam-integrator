98c03c0288e058ee5fc57853ac5bac29550d5521
Eric Rosas <ericdrosas@gmail.com>
2023-04-18 16:12:17 -0600

    Added migration to update `fields` table EPO-7944

---
+++ src/migrations/m230412_233209_namespace_change_migration.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace lsst\dam\migrations;
+
+use Craft;
+use craft\db\Migration;
+
+/**
+ * m230412_233209_namespace_change_migration migration.
+ */
+class m230412_233209_namespace_change_migration extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp(): bool
+    {
+        echo "m230412_233209_namespace_change_migration executing...\n";
+
+        $this->update('{{%fields}}', [
+            "type" => "lsst\dam\\fields\DAMAsset",
+
+    ],[
+            "type" => "rosas\dam\\fields\DAMAsset"
+    ]);
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown(): bool
+    {
+        echo "m230412_233209_namespace_change_migration cannot be reverted.\n";
+        return false;
+    }
+}
